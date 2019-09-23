1dfd8b27342ebe4233716514ade33d20f62567b6
Placide IRANDORA <placideirandora@gmail.com>
2019-09-23 21:41:08 +0200

    Create the Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2019 Placide IRANDORA</p>
+        </footer>
+    )
+};
+
+export default Footer;
