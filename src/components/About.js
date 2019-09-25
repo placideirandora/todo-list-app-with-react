384132e097545bff2734d8267c1e187404a2c433
Placide IRANDORA <placideirandora@gmail.com>
2019-09-25 19:16:46 +0200

    Add About page component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <p className="description">To-Do List is a web application that helps you create todos and keep track of them</p>
+        </div>
+    )
+};
+
+export default About;
