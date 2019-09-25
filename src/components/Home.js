742dfd5860cf5cb44be23ae207356984ec7878ab
Placide IRANDORA <placideirandora@gmail.com>
2019-09-25 19:24:47 +0200

    Add Home page component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className="description">Manage your todos in an effective and efficient manner!</p>
+        </div>
+    )
+};
+
+export default Home;
