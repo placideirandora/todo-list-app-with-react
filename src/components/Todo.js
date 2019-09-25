ad2a3edea580f69e6de1beaa8bf61b1a2210a08a
Placide IRANDORA <placideirandora@gmail.com>
2019-09-25 19:26:13 +0200

    Add Todos page component

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return (
+            <div>
+                <p className="description">Get ready to create your todos</p>
+            </div>
+        )
+    }
+}
+
+export default Todos;
