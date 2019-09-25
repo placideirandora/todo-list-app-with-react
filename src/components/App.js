ed41a31fdd650e8cd41e409dcc0c36062cd59686
Placide IRANDORA <placideirandora@gmail.com>
2019-09-25 19:28:59 +0200

    Add a navigation menu and handle the corresponding routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from './Nav';
 import '../styles/App.scss'
-import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Todos from './Todo';
 import Footer from './Footer';
 
 class App extends Component {
     render() {
         return(
-            <div className="App">
-                <Header/>
-                <p className="description">Manage and track your todos in an effective and efficient manner!</p>
-                <button className="start-btn">Start</button>
-                <Footer/>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                        <Navbar />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/Todos" component={Todos} />
+                            <Route path="/About" component={About} />
+                        </Switch>
+                        <Footer />
+                </div>
+            </BrowserRouter>
         )
     }
 }
