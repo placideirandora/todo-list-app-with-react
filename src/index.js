86f23aa16f4ce2b049b04349759306db57ff7593
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:37:07 +0200

    Update the store name and format the code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import rootReducer from './reducers/rootReducer';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const todoStore = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={todoStore}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
