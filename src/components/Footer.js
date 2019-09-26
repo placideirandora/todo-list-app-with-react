15603339ebcde02659a60d27207b5eb3a082cd14
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:20:30 +0200

    Format the code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../styles/Footer.scss';
 
 const Footer = () => {
-    return (
-        <footer>
-            <p>Copyright &copy; 2019 Placide IRANDORA</p>
-        </footer>
-    )
+  return (
+    <footer>
+      <p>Copyright &copy; 2019 Placide IRANDORA</p>
+    </footer>
+  );
 };
 
 export default Footer;
