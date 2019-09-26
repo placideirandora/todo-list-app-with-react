40c3c6e6858c39c98e7943976c2228f23a50c40f
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:15:17 +0200

    Update the about content and format the code

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const About = () => {
-    return (
-        <div>
-            <p className="description">To-Do List is a web application that helps you create todos and keep track of them</p>
-        </div>
-    )
+  return (
+    <div>
+      <p className="description">
+        A web application that helps you create todos and keeps track of them
+      </p>
+    </div>
+  );
 };
 
 export default About;
