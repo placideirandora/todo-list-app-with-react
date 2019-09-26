f14145f56caa9080f5231af175136dfd003512e3
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 21:07:38 +0200

    Format the configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js"
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -13,18 +13,18 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
       {
-        test:/\.(s*)css$/,
-        use:['style-loader','css-loader', 'sass-loader']
-     }
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html"
+      template: './public/index.html'
     })
   ],
   devServer: {
