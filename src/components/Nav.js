2d34179abe17f25a0d1802674737bb5abbcf7ec5
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:22:01 +0200

    Add a navigation menu for adding todos and format the code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/Nav.scss";
+import '../styles/Nav.scss';
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/" className="logo">TO-DO LIST</Link>
-      <Link to="/About" className="menu last">ABOUT</Link>
-      <Link to="/Todos" className="menu">TODOS</Link>
+      <Link to="/" className="logo">
+        TO-DO LIST
+      </Link>
+      <Link to="/About" className="menu last">
+        ABOUT
+      </Link>
+      <Link to="/AddTodos" className="menu second">
+        ADD TODOS
+      </Link>
+      <Link to="/Todos" className="menu">
+        VIEW TODOS
+      </Link>
     </nav>
   );
 };
