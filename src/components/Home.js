7ea5f47b6c34978f5992e7e0365dc9c410bff61d
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:20:55 +0200

    Format the code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <p className="description">Manage your todos in an effective and efficient manner!</p>
-        </div>
-    )
+  return (
+    <div>
+      <p className="description">
+        Manage your todos in an effective and efficient manner!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
