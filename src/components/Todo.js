447c37818f50bb942f14579d410419d2fe1e6739
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:23:50 +0200

    Implement the feature of adding todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../styles/Todo.scss';
 
 class Todos extends Component {
-    render() {
+  render() {
+    const todoList = this.props.todos.length ? (
+      this.props.todos.map(todo => {
         return (
-            <div>
-                <p className="description">Get ready to create your todos</p>
-            </div>
-        )
-    }
+          <div key={todo.id}>
+            <p className="todos">
+              {todo.content} <button>Complete</button>
+              <hr />
+            </p>
+          </div>
+        );
+      })
+    ) : (
+      <p className="no-todos"> You don't have any todos </p>
+    );
+    return <div>{todoList}</div>;
+  }
 }
 
-export default Todos;
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps)(Todos);
