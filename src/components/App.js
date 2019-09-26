7eaff2f5e51f6205f40e6edb8690fd82f3e86f68
Placide IRANDORA <placideirandora@gmail.com>
2019-09-26 18:19:28 +0200

    Add a route for adding todos and format the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './Nav';
-import '../styles/App.scss'
+import '../styles/App.scss';
 import Home from './Home';
 import About from './About';
 import Todos from './Todo';
 import Footer from './Footer';
+import AddTodos from './AddTodo';
 
 class App extends Component {
-    render() {
-        return(
-            <BrowserRouter>
-                <div className="App">
-                        <Navbar />
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/Todos" component={Todos} />
-                            <Route path="/About" component={About} />
-                        </Switch>
-                        <Footer />
-                </div>
-            </BrowserRouter>
-        )
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Todos" component={Todos} />
+            <Route path="/AddTodos" component={AddTodos} />
+            <Route path="/About" component={About} />
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
